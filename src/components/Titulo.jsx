08dc573635c27aaa145c0791dc16676bfeb0b0ce
maru.mendoza <mariana.e.mendoza@gmail.com>
2024-09-09 16:36:28 -0300

    agregar función anónima porque se tiene una linea de código

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -3,13 +3,17 @@ import { useState } from "react";
 const Titulo = () => {
 //  lógica del componente
 const [msg,setMsg] = useState('Hello my Friend!');
-const mostrarMsg = () =>{
-    setMsg('Hello my Friend (from changed state)!')
-}
+// const mostrarMsg = () =>{
+//     setMsg('Hello my Friend (from changed state)!')
+// }
   return (
     <section className="container my-5 bg bg-danger text-white text-center">
       <h1>{msg}</h1>
-      <button className="btn btn-primary" onClick={mostrarMsg}>Botón</button>
+      <button
+        className="btn btn-primary"
+        onClick={() => setMsg("Hello my Friend (from changed state)!")}>
+        Botón
+      </button>
     </section>
   );
 };
